3e3238b596640dd63fd8c4dac708f9dec4343696
Emerson Matheus Brancher <emerson.brancher@paytrack.com.br>
2022-09-10 22:55:24 -0300

    [CM-231] - Adicionar vizinho

--- src/main/java/modelo/Campo.java
+++ src/main/java/modelo/Campo.java
@@ -14,9 +14,29 @@ public class Campo {
 
     private List<Campo> vizinhos = new ArrayList<>();
 
-    Campo(int linha, int coluna){
+    public Campo(int linha, int coluna){
         this.linha = linha;
         this.coluna = coluna;
+    }
+
+    public boolean adicionarVizinho(Campo vizinho) {
+        boolean linhaDiferente = linha != vizinho.linha;
+        boolean colunaDiferente = coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferente && colunaDiferente;
+
+        int deltaLinha = Math.abs(linha - vizinho.linha);
+        int deltaColuna = Math.abs(linha - vizinho.coluna);
+        int detalGeral = deltaColuna + deltaLinha;
+
+        if (detalGeral == 1 && !diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else if (detalGeral == 2 && diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
---
+++ src/test/java/com/brancherapps/cm/modelo/CampoTest.java
@@ -0,0 +1,82 @@
+package com.brancherapps.cm.modelo;
+
+
+import modelo.Campo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTest {
+
+    private Campo campo = new Campo(3,3);
+
+    @BeforeEach
+    void iniciarCampo() {
+        Campo campo = new Campo(3, 3);
+    }
+
+    @Test
+    void testeVizinhoDistancia1Esquerda(){
+        Campo vizinho = new Campo(3, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia1Direita(){
+        Campo vizinho = new Campo(3, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia1Emcima(){
+        Campo vizinho = new Campo(2, 3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia1Embaixo(){
+        Campo vizinho = new Campo(4, 3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia2EmcimaEsquerda(){
+        Campo vizinho = new Campo(2, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia2EmcimaDireita(){
+        Campo vizinho = new Campo(2, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia2EmBaixoEsquerda(){
+        Campo vizinho = new Campo(2, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeVizinhoDistancia2EmBaixoDireita(){
+        Campo vizinho = new Campo(4, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    void testeNaoVizinho(){
+        Campo vizinho = new Campo(1, 1);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        Assertions.assertFalse(resultado);
+    }
+
+
+}
